1e9b5ab110c1537a8a79d0cca4de97dbd558bf73
이혜연 <dlgpdus0416@naver.com>
2022-05-19 14:31:03 +0900

    chapter8, chapter9 연습문제 풀이

---
+++ java/chapter09/src/abstractex/Car.java
@@ -0,0 +1,9 @@
+package abstractex;
+
+public abstract class Car {
+    public abstract void run();
+    public abstract void refuel();
+    public void stop(){
+        System.out.println("차가 멈춥니다.");
+    }
+}
